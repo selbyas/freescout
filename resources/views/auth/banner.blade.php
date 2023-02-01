f3068c421837475e2a44435c16c19cb342b7439d
Knut Leborg <knut@lepa.no>
2023-02-01 19:52:05 +0100

    update branding

--- resources/views/auth/banner.blade.php
+++ resources/views/auth/banner.blade.php
@@ -1,3 +1,3 @@
 <div class="banner" style="margin-bottom: 30px;">
-    <img src="@filter('login.banner', asset('img/banner.png'))" width="183" alt="Selby Logo Banner">
+    <img src="@filter('login.banner', asset('img/banner.png'))" alt="Selby Logo Banner">
 </div>
